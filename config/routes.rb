607555bf78eab91ebea4b23722deda68bb2c03c9
nozomi.handa <parsley1102@gmail.com>
2020-06-12 12:15:31 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
+  
+  resources :microposts, only: [:create, :destroy]
 end
+
